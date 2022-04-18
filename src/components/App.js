2ea15c93a327f6f7834a93d15b8404099a8216c2
anirudh <anirudh7186@gmail.com>
2022-04-18 02:11:03 +0000

    first commit-basic bootstrap template

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Navbar'
+import Time from './Time'
+import Footer from './Footer'
+
+function App() {
+    return (
+        <>
+            <Navbar />
+            <Time />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Time.js
@@ -0,0 +1,19 @@
+
+function Time() {
+    return (
+        <>
+            {/* <div className="container">
+                <h3>Start Time</h3>
+                <input type="date" />
+                <input type="time"/>
+            </div>
+            <div className="container">
+                <h3>End Time</h3>
+                <input type="date" />
+                <input type="time"/>
+            </div> */}
+        </>
+    )
+}
+
+export default Time
