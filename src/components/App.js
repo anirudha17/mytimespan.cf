05d15171b7609aabf8478c55175844f7712e7cca
anirudh <anirudh7186@gmail.com>
2022-04-21 04:14:13 +0000

    minor changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './Navbar'
 import Time from './Time'
 import Footer from './Footer'
+import About from './About'
 
 function App() {
     return (
         <>
             <Navbar />
-            <Time />
+            <Routes>
+                <Route path="/" element={<Time />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
             <Footer />
         </>
     );
